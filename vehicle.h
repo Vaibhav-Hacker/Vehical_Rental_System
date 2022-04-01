95a5573de2f4c3baf6bd256d0932dcf937a3dc3d
Vaibhav Tanwar <79646851+Vaibhav-Hacker@users.noreply.github.com>
2022-04-01 20:57:20 +0530

    Add files via upload

---
+++ vehicle.h
@@ -0,0 +1,36 @@
+#ifndef vehicle_h
+#define vehicle_h
+
+#include "date.h"
+#include "storable.cpp"
+#include <string>
+
+using namespace std;
+
+typedef enum { bike = 1, car = 2, towera = 3} VehicleType;
+
+class Vehicle : public Storable {
+    private:
+        string registrationNumber;
+        VehicleType type;
+        int seats;
+        string companyName;
+        double pricePerKm;
+
+        Date PUCExpirationDate;
+
+    public:
+        Vehicle(string registrationNumber, VehicleType type, int seats, string companyName, double pricePerKm, Date PUCExpirationDate, long recordId = 0);
+        string getRegistrationNumber() const;
+        VehicleType getVehicleType() const;
+        string getVehicleTypeName() const;
+        double getpricePerKm() const;
+        Date getPUCExpirationDate() const;
+
+        void setPricePerKm(double newPrice);
+        void display() const;
+        string toString() const;
+        void setDataFrom(Storable *s);
+};
+
+#endif
